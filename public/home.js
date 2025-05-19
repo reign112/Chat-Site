c63847f8637311e8968b9e1e75147abad16d28d1
reign112 <jethroreign24@gmail.com>
2025-05-19 18:16:49 +0100

    Update home.js

--- public/home.js
+++ public/home.js
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <div class="placeholder-content">
             <i class="fas fa-home fa-3x"></i>
             <h3>Home Dashboard</h3>
-            <p>This is your personal dashboard. View your account information and settings here.</p>
+            <p>We're humble, and value learning over being right. We embrace delivering and receiving candid feedback, and see every setback as an opportunity to learn.</p>
           </div>
         `
         break
